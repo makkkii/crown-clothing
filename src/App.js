e1a5f5c9ef18bc284937380b3997120906641195
Makki <makkii.dev@gmail.com>
2019-10-21 00:01:31 +0530

    Added basic navigation.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from 'react';
 import './App.css';
 
 import HomePage from './pages/homepage/HomePage';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    // <div className="App">
+    //   <HomePage />
+    // </div>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route  path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
