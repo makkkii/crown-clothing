5280268f89fd6adca9ab77b02016c0b60f96b6b0
Makki <makkii.dev@gmail.com>
2019-11-12 18:31:08 +0530

    Added firebase config.

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDR4HHTkv3C6cPpjqp2nPNxmdfRd9M-Mpo",
+  authDomain: "crown-db-6254.firebaseapp.com",
+  databaseURL: "https://crown-db-6254.firebaseio.com",
+  projectId: "crown-db-6254",
+  storageBucket: "crown-db-6254.appspot.com",
+  messagingSenderId: "978570310937",
+  appId: "1:978570310937:web:c9dbe272db580322938188",
+  measurementId: "G-Q4DSTXNLJ8"
+};
+
+export default firebaseConfig;
